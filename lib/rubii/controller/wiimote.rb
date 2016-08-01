6fa489568091b2c729066c168c36f40a7ea8c667
ppibburr <tulnor33@gmail.com>
2016-08-01 03:16:04 -0400

    Update wiimote.rb

---
+++ lib/rubii/controller/wiimote.rb
@@ -0,0 +1,60 @@
+module Rubii
+  module WiiMote
+    class Controller < Rubii::Controller
+      class Led < Rubii::Component
+        def set i
+
+        end
+ 
+        def get
+  
+        end
+ 
+        def blink rate=1
+ 
+        end
+      end
+
+      class Buttons < Rubyii::Controller::Buttons
+      end
+
+      class Acc < Axis
+        def read
+          @values = controller.state.acc || @values || [0,0,0]
+          super
+        end
+      end
+
+      class IR < Axis
+        def read
+          @values = controller.state.ir[0] || @values || [0,0,0]
+          super
+        end
+      end
+
+      class Nunchuk < Axis
+        def read
+          @values = controller.state.nunchuk || @values || [0,0,0]
+          super
+        end
+      end
+
+      attr_reader :wiimote, :state
+      def initialize rpt_mode = RPT_ACC | RPT_BUTTONS | REPORT_IR
+        @wiimote = WiiMote.new()
+        wiimote.rpt_mode = rpt_mode
+
+        @buttons       = Buttons.new
+
+        @axi[:acc]     = Acc.new
+        @axi[:ir]      = IR.new
+        @axi[:nunchuk] = Nunchuk.new
+      end
+
+      def update
+        wiimote.request_status
+        @state = wiimote.get_state
+      end
+    end
+  end
+end
